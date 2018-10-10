577f9bce56124e18320c048896fa9b23d688ef05
Pierre-Olivier NAHOUM <ponahoum@me.com>
2018-10-10 20:39:48 +0200

    updating save strategry

--- stacks/sites-ginfo-apache/save-strategy/entrypoint.sh
+++ stacks/sites-ginfo-apache/save-strategy/entrypoint.sh
@@ -4,8 +4,8 @@ rm -f /etc/cron.d/cron-save
 ln -s /var/www/crontab /etc/cron.d/cron-save
 chmod 0644 /var/www/crontab
 
-rm -f /.env
-printenv | sed 's/^\(.*\)$/export \1/g' > /.env
+rm -f /etc/environment
+printenv | sed 's/^\(.*\)$/export \1/g' > /etc/environment
 
 cron
 
