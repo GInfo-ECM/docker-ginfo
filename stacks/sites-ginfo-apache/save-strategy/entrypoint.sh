40ad6ad83fe5f9c23b966249c237ce706f0e109e
Pierre-Olivier NAHOUM <ponahoum@me.com>
2018-10-10 23:06:55 +0200

    Save strategy update

--- stacks/sites-ginfo-apache/save-strategy/entrypoint.sh
+++ stacks/sites-ginfo-apache/save-strategy/entrypoint.sh
@@ -4,9 +4,8 @@ rm -f /etc/cron.d/cron-save
 ln -s /saves/crontab /etc/cron.d/cron-save
 chmod 0644 /saves/crontab
 
+mkdir -p /saves/backups
 rm -f /etc/environment
 printenv | sed 's/^\(.*\)$/export \1/g' > /etc/environment
 
-cron
-
-tail -f /var/log/cron.log
+cron && tail -f /var/log/cron.log
