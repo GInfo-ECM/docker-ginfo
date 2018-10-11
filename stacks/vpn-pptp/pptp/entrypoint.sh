b936e612f5ffe4d3f4adc0596cfea8a2effe4d80
Pierre-Olivier NAHOUM <ponahoum@me.com>
2018-10-11 10:15:42 +0200

    VPN PPTP

--- stacks/vpn-pptp/pptp/entrypoint.sh
+++ stacks/vpn-pptp/pptp/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -f /etc/ppp/chap-secrets
-echo ${USERNAME}"    *           "${PASSWORD}"    *" >> /etc/ppp/chap-secrets
+echo "$USERNAME    *           $PASSWORD    *" >> /etc/ppp/chap-secrets
 
 pptpd --fg
