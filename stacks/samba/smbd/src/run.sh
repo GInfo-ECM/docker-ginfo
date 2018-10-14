cf5acd7cc5b523664a24b93e8c90729cf9506db4
Pierre-Olivier NAHOUM <ponahoum@me.com>
2018-10-14 17:43:40 +0200

    samba

--- stacks/samba/smbd/src/run.sh
+++ stacks/samba/smbd/src/run.sh
@@ -12,8 +12,18 @@ if [ ! -f /var/lib/extrausers/group ]; then
   touch /var/lib/extrausers/group
 fi
 
+
 if [ $(dpkg-query -W -f='${Status}' samba 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
   /opt/rootlogin-fileserver/prepare_samba.sh
+fi
+
+if [ ! -d /home/associations ]; then
+  #Création du dossier commun aux association
+  mkdir /home/associations
+  /opt/rootlogin-fileserver/manage.sh add_group associations
+  useradd -m -g associations sambaadmin
+  chown -R sambaadmin:associations /home/associations
+  chmod -R 770 /home/associations
 fi
 
 if [ "$1" == "manage" ]; then
