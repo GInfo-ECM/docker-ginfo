44a7dae4929320355199784d66bff64bd2c4e12c
Pierre-Olivier NAHOUM <ponahoum@me.com>
2018-10-21 17:11:20 +0200

    postfix

---
+++ stacks/serveur-mail/postfix/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Script qui envoie un mail si la file d'attente est surchargée
+service postfix start
+
+while true
+do
+    queue=$(mailq | grep -c "^[A-F0-9]")
+
+	if [ "${queue:-0}" -ge "5" ]; then
+
+    echo $(mailq)
+    sleep 600
+    mailing "Activite anormale sur le serveur mail" "Une activité anormale a été détectée sur le serveur mail : \n"$(mailq)
+
+    else
+
+    echo $(mailq)
+    sleep 60
+
+    fi
+
+done
+
+
+mailing(){
+mail -s $1 monitoring@domain.mail.ltd -r bor+ginfo@centrale-marseille.fr -a "From: ginfo-monitoring" <<< $2
+}
