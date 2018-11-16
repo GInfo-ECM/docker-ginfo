7d5948ad64efea75eb26e210a87704893a385f8a
Nathanaël Eon <neon@ec-m.fr>
2018-11-16 21:35:27 +0100

    changed update log

--- stacks/wordpress-assos/wordpress/update.sh
+++ stacks/wordpress-assos/wordpress/update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-wp core update >> /var/log/wpupdate.log
-wp core update-db >> /var/log/wpupdatedb.log
+wp core update >> /var/log/wpupdate.log 2>&1
+
+wp core update-db >> /var/log/wpupdatedb.log 2>&1
