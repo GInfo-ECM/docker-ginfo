5a85d69468748da0bdfd23b604f43b57fb69b941
Nathanaël Eon <neon@ec-m.fr>
2018-11-16 21:57:43 +0100

    Added path for wp 	modifié :         Dockerfile 	modifié :         crontab 	modifié :         update.sh

--- stacks/wordpress-assos/wordpress/update.sh
+++ stacks/wordpress-assos/wordpress/update.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-wp core update >> /var/log/wpupdate.log 2>&1
+wp cli update >> /var/log/wpcliupdate.log 2>&1
 
-wp core update-db >> /var/log/wpupdatedb.log 2>&1
+wp core update --path="/var/www/html" >> /var/log/wpupdate.log 2>&1
+
+wp core update-db --path="/var/www/html" >> /var/log/wpupdatedb.log 2>&1
