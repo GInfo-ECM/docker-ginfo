3df4ba807f8072c0db9dfef4f82e49dbe3e596fd
Pierre-Olivier Nahoum <ponahoum@me.com>
2019-01-18 16:14:10 +0100

    WP config for using proxy

---
+++ stacks/wordpress-assos/wordpress/proxy_infos.inc.php
@@ -0,0 +1,6 @@
+/* Configure proxy Server */
+define('WP_PROXY_HOST', 'host_proxy');
+define('WP_PROXY_PORT', '3128');
+define('WP_PROXY_USERNAME', 'username_proxy');
+define('WP_PROXY_PASSWORD', 'password_proxy');
+define('WP_PROXY_BYPASS_HOSTS', 'localhost');
