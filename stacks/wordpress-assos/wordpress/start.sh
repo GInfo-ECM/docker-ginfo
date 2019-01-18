f589a82c1a1b49d9607f93bff4a2769d2aeb836d
Pierre-Olivier Nahoum <ponahoum@me.com>
2019-01-18 23:21:25 +0100

    WP arguments

--- stacks/wordpress-assos/wordpress/start.sh
+++ stacks/wordpress-assos/wordpress/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if ! grep -q "WP_PROXY_HOST" "/usr/src/wordpress/wp-config-sample.php"; then
-  echo "require_once('/var/www/proxy_infos.inc.php');" >> /usr/src/wordpress/wp-config-sample.php
+  echo "include('/var/www/proxy_infos.inc.php');" >> /usr/src/wordpress/wp-config-sample.php
 fi
