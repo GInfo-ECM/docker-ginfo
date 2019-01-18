ea749141bf550a92248a4e974d629dfbe43edee3
Pierre-Olivier Nahoum <ponahoum@me.com>
2019-01-18 18:13:06 +0100

    WP arguments

--- stacks/wordpress-assos/wordpress/entrypoint.sh
+++ stacks/wordpress-assos/wordpress/entrypoint.sh
@@ -5,5 +5,5 @@ proxy_string="WP_PROXY_HOST"
 /usr/local/bin/docker-entrypoint.sh &&
 
 if ! grep -q $proxy_string "/var/www/html/wp-config.php"; then
-  echo "include('/var/www/html/proxy_infos.inc.php');" >> /var/www/html/wp-config.php
+  echo "include('/var/www/proxy_infos.inc.php');" >> /var/www/html/wp-config.php
 fi
