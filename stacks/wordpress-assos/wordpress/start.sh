821c0d469c30952a6555334d6de9cefc811df582
Pierre-Olivier Nahoum <ponahoum@me.com>
2019-01-18 23:02:52 +0100

    WP arguments

--- stacks/wordpress-assos/wordpress/start.sh
+++ stacks/wordpress-assos/wordpress/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sleep 10
-if ! grep -q "WP_PROXY_HOST" "/var/www/html/wp-config.php"; then
-  echo "require_once(ABSPATH . '/var/www/proxy_infos.inc.php');" >> /var/www/html/wp-config.php
-fi
+#sleep 10
+#if ! grep -q "WP_PROXY_HOST" "/var/www/html/wp-config.php"; then
+#  echo "require_once(ABSPATH . '/var/www/proxy_infos.inc.php');" >> /var/www/html/wp-config.php
+#fi
 
 apache2-foreground
