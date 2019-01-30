8b8cce7b827b65386ca2ff2a65a9e0a55f6b5777
Pierre-Olivier Nahoum <ponahoum@me.com>
2019-01-30 10:34:32 +0100

    Static stack for asso

--- stacks/sites-statiques/apache/entrypoint.sh
+++ stacks/sites-statiques/apache/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-/var/www/startup.sh &&
-tail -f ${APACHE_LOG_DIR}/access.log &
-tail -f ${APACHE_LOG_DIR}/error.log &
-httpd-foreground
+/var/www/startup.sh && apache2-foreground
+#tail -f ${APACHE_LOG_DIR}/access.log &
+#tail -f ${APACHE_LOG_DIR}/error.log &
