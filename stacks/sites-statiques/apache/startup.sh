04e2952205b9a7e82e0154a9c769d275c59484c0
Pierre-Olivier Nahoum <ponahoum@me.com>
2019-02-13 15:05:51 +0100

    statique log update

--- stacks/sites-statiques/apache/startup.sh
+++ stacks/sites-statiques/apache/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #Gestion de la création du dossier par défaut (si jamais supprimé ou premier démarrage)
 mkdir -p ${APACHE_DOCUMENT_ROOT}
-
+#Importation des variables d'environnement
+> /etc/environment
+echo "export APACHE_LOG_DIR=${APACHE_LOG_DIR}" >> /etc/environment
 #Autres instructions
