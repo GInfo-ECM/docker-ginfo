9043511e64466cdc0528b5090a645a7ba5670192
Pierre-Olivier Nahoum <ponahoum@me.com>
2019-02-13 23:00:21 +0100

    wp starting

--- stacks/wordpress-assos/wordpress/start-command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-touch ${APACHE_LOG_DIR}/access.log &&
-touch ${APACHE_LOG_DIR}/error.log &&
-tail -f ${APACHE_LOG_DIR}/access.log &
-tail -f ${APACHE_LOG_DIR}/error.log &
-apache2-foreground
