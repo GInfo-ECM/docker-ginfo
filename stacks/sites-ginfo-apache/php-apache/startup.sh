140480ae8cca1ae2c92b21912f183623639edd13
Pierre-Olivier Nahoum <ponahoum@me.com>
2019-02-13 21:08:25 +0100

    Log for apache php stack

--- stacks/sites-ginfo-apache/php-apache/startup.sh
+++ stacks/sites-ginfo-apache/php-apache/startup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+
 #Gestion de la création du dossier par défaut (si jamais supprimé ou premier démarrage)
 mkdir -p ${APACHE_DOCUMENT_ROOT}
+
+#Si l'hébergement est vide, on rajoute un index par défaut (premier démarrage)
+if [ -z "$(ls -A ${APACHE_DOCUMENT_ROOT})" ]; then
+   cp /etc/apache2/index_default.php ${APACHE_DOCUMENT_ROOT}/index.php
+fi
 
 #Gestion de PhpMyAdmin au démarrage - ne pas supprimer (sauf si vous ne voulez plus de PhpMyAdmin)
 htpasswd -b -c /phpmyadmin/.htpasswd ${DB_USER} ${DB_PASSWORD}
 rm -f ${APACHE_DOCUMENT_ROOT}/phpmyadmin-ginfo
 ln -s /phpmyadmin ${APACHE_DOCUMENT_ROOT}/phpmyadmin-ginfo
-rm -R /phpmyadmin/tmp
+rm -Rf /phpmyadmin/tmp
 mkdir /phpmyadmin/tmp
 chmod 777 /phpmyadmin/tmp
 rm -f /phpmyadmin/config.inc.php
