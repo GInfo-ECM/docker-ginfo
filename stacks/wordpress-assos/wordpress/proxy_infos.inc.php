5a7d425715bdefec5549846aac9159036447db5b
Pierre-Olivier Nahoum <ponahoum@me.com>
2019-02-15 15:30:45 +0100

    wordpress proxy infos

--- stacks/wordpress-assos/wordpress/proxy_infos.inc.php
+++ stacks/wordpress-assos/wordpress/proxy_infos.inc.php
@@ -1,7 +1,5 @@
 <?php
 define('WP_PROXY_HOST', getenv("PROXY_HOST"));
 define('WP_PROXY_PORT', '3128');
-define('WP_PROXY_USERNAME', getenv("PROXY_USER"));
-define('WP_PROXY_PASSWORD', getenv("PROXY_PASS"));
 define('WP_PROXY_BYPASS_HOSTS', 'localhost');
 ?>
