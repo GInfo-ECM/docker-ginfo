7f37923f89426f07f03b5388412b60558f228b73
Pierre-Olivier Nahoum <ponahoum@me.com>
2019-02-16 15:03:32 +0100

    Update cron

--- stacks/wordpress-assos/wordpress/update.sh
+++ stacks/wordpress-assos/wordpress/update.sh
@@ -1,70 +1,100 @@
 #!/bin/bash
-
-############### Printing in a log file ################
+############ Définition des variables ############
 logfile=/var/log/wpupdate-last.log
 mailfilehtml=/var/log/wpupdate-last.log.html
+rm -rf $logfile
+rm -rf $mailfilehtml
 exec > $logfile 2>&1
-
-#Installer
 siteurl=$(wp option get siteurl --path='/var/www/html')
 email=$(wp option get admin_email --path='/var/www/html')
-email_list="none@ec-m.fr"
+#Mailing list de suivi de la plateforme
+email_list="liste@ec-m.fr"
 d=$(date)
-error="Error"
-success="Success"
+errorCount=0
 
-#Execute a wp cli command and checks if it returned an error or not (and colors it)
+#Execute une commande Wordpress et défini si la sortie contient une erreur ou non
 evalCommand () {
     eval $1 > /tmp/tempwpfile 2>&1
     cmdoutput=$(cat /tmp/tempwpfile)
     if [[ $cmdoutput == *"Error"* ]]; then
         echo "\e[0m\e[31m$(< /tmp/tempwpfile)\e[0m"
+        errorCount=$((errorCount+1))
     else
         echo "\e[0m\e[32m$(< /tmp/tempwpfile)\e[0m"
     fi
     rm /tmp/tempwpfile
 }
 
-#########################
+############ Début des tâches routinières ############
 cd /var/www/html
 
 echo "\e[1mRapport des tâches routinières du "$d" pour le site "$siteurl"\e[0m"
 
-##### No need to update wp-cli
-# wp cli update >> /var/log/wpcliupdate.log 2>&1
+echo "strerror"
+
+echo "\n\e[1m------ WP CLI ------\e[0m"
+evalCommand 'wp cli update'
 
 echo "\n\e[1m------ Core Wordpress, plugins et templates ------\e[0m"
 
-echo "\n\e[34m------ Core Wordpress ------\e[0m"
+echo "\n\e[34m-- Core Wordpress --\e[0m"
 evalCommand 'wp core update --path="/var/www/html"'
 
-echo "\n\e[34m------ DB du Core ------\e[0m"
+echo "\n\e[34m-- DB du Core --\e[0m"
 evalCommand 'wp core update-db --path="/var/www/html"'
 
-echo "\n\e[34m------ Suppression des thèmes dépréciés ------\e[0m"
-themeremove=$(/usr/lib/wp-theme-remove)
-[ -z "$themeremove" ] && echo "\e[0m\e[32m OK : Aucun thème n'a été supprimé\e[0m" || echo "\e[31m"$themeremove"\e[0m\n"
-
-echo "\n\e[34m------ Suppression des plugins dépréciés ------\e[0m"
-pluginremove=$(/usr/lib/wp-plugin-remove)
-[ -z "$pluginremove" ] && echo "\e[0m\e[32m OK : Aucun plugin n'a été supprimé\e[0m" || echo "\e[31m"$pluginremove"\e[0m\n"
-
-echo "\n\e[34m------ Mise à jour des thèmes ------\e[0m"
+echo "\n\e[34m-- Mise à jour des thèmes --\e[0m"
 SHELL_PIPE=0 wp theme list
 evalCommand 'wp theme update --all'
 
-echo "\n\e[34m------ Mise à jour des plugins ------\e[0m"
+echo "\n\e[34m-- Mise à jour des plugins --\e[0m"
 SHELL_PIPE=0 wp plugin list
 evalCommand 'wp plugin update --all'
 
+echo "\n\e[34m-- Suppression des thèmes dépréciés --\e[0m"
+themeremove=$(/usr/lib/wp-theme-remove)
+if [ -z "$themeremove" ]
+then
+      echo "\e[0m\e[32m OK : Aucun thème n'a été supprimé\e[0m"
+else
+      echo "\e[31m"$themeremove"\e[0m\n";
+      errorCount=$((errorCount+1))
+fi
+
+echo "\n\e[34m-- Suppression des plugins dépréciés --\e[0m"
+pluginremove=$(/usr/lib/wp-plugin-remove)
+if [ -z "$pluginremove" ]
+then
+      echo "\e[0m\e[32m OK : Aucun plugin n'a été supprimé\e[0m"
+else
+      echo "\e[31m"$pluginremove"\e[0m\n";
+      errorCount=$((errorCount+1))
+fi
+
 echo "\n\n\e[1m------ Informations supplémentaires ------\e[0m"
-echo "\n\e[34m------ Espace disque disponible ------\e[0m"
+echo "\n\e[34m-- Espace disque disponible --\e[0m"
 df /
 
+echo "\n\n\e[1mCe mail a été envoyé à $email et à la mailing list du GInfo $email_list"
+echo "Cette plateforme est gérée par le GInfo, merci de le contacter en cas de problème avec ce rapport."
 
-#Generation logfile
-header="To:"$email","$email_list"\nSubject:Rapport des tâches routinières du "$d" pour le site "$siteurl"\nMIME-Version: 1.0\r\nContent-Type: text/html; charset=utf-8"
+
+############ Envoi du rapport par mail ############
+if [ "$errorCount" != "0" ]
+then
+      errorHeader="[Important rapport]"
+      errorString="\\\e[31mCe rapport contient $errorCount erreur(s) ou message(s) d'alerte requérant votre attention \\\e[0m"
+else
+      errorHeader="[Rapport]"
+      errorString=""
+fi
+
+
+header="To:$email,$email_list\nSubject:$errorHeader Rapport des tâches routinières du $d pour le site $siteurl\nMIME-Version: 1.0\r\nContent-Type: text/html; charset=utf-8"
 echo -e $header > $mailfilehtml
+sed -i -e "s/strerror/$errorString/g" $logfile
 echo -e "$(< $logfile)" | ansi2html >> $mailfilehtml
 sendmail -t < /var/log/wpupdate-last.log.html
 
+############ Nettoyage ############
+rm $mailfilehtml
