4fbe5ac0ebedcf70891ae51bd7cc93c010d260a5
Laura VIETOR <paul.vietor@centrale-med.fr>
2025-02-25 15:22:17 +0100

    Use the Wordpress API to fetch plugin & theme info

--- stacks/wordpress-assos/wordpress/plugins-remove.sh
+++ stacks/wordpress-assos/wordpress/plugins-remove.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# For each plugin check if it fits our criterions (updated less than 3y ago, more that 100 active_installs
-# To test you can use plugin "smart-throttle"
+# For each plugin check if it fits our criterions (updated less than 4y ago, more that 300 active_installs
 
 TIMELAPSE="4 years"
 ACTIVEINSTALLS=300
@@ -12,76 +11,70 @@ ACTIVEINSTALLS=300
 declare -a NOTHING_DELETED
 NOTHING_DELETED=true
 
-delete_plugin () {
-    echo "Warning ! Uninstalling plugin $1"
-    wp plugin uninstall --deactivate "$1"
+delete_plugin() {
+	echo "Warning ! Uninstalling plugin $1"
+	wp plugin uninstall --deactivate "$1"
 }
 
-
-while read plug; do 
+while read plug; do
 
 	if [[ $plug == *".php" ]]; then
-	    echo "Didn't update plugin $plug because it is droppin"
-	    continue 2
-    fi
+		echo "Didn't update plugin $plug because it is droppin"
+		continue 2
+	fi
 
-    if [[ $plug == "" ]]; then
-	    echo "No plugin installed"
-	    continue 2
-    fi
+	if [[ $plug == "" ]]; then
+		echo "No plugin installed"
+		continue 2
+	fi
 
 	# Skip the part of the list we want to keep
-	for val in $LIST_PLUGINS_KEPT
-	do
-		if [ "${plug}x" = "${val}x" ]
-		then
-		    continue 2 # Next path
+	for val in $LIST_PLUGINS_KEPT; do
+		if [ "${plug}x" = "${val}x" ]; then
+			echo "Plugin ${plug} in LIST_PLUGINS_KEPT, skipping."
+			continue 2 # Next plugin
 		fi
 	done
 
-	declare -a isNotInDb
-	isNotInDb=true
+	resp=$(curl -s "https://api.wordpress.org/plugins/info/1.2/?action=plugin_information&slug=${plug}")
 
-	research=`wp plugin search $plug --fields=slug,last_updated,active_installs --format=json`
-
-	retval=$?
-	# Checking if error (if so wp already prints something so we just prevent the deletion)
-	if [ $retval -eq 0 ]
-	then 	
-		while read i;
-		do
-			slug=$(echo `jq -r '.slug' <<< ${i}`)
-			date=$(echo `jq -r '.last_updated' <<< ${i}`)
-			active=$(echo `jq -r '.active_installs' <<< ${i}`)
-
-			if [[ "$slug" == "$plug" ]]
-			then
-			    d1=$(date --utc --date="$(date)" +"%Y-%m-%d %H:%M:%S")
-			    d2=$(date --utc --date="$date+$TIMELAPSE" +"%Y-%m-%d %H:%M:%S")
-
-			    if [[ "$d2" < "$d1" ]] || [[ $active -lt $ACTIVEINSTALLS ]]  # date criterion
-			    then
-				delete_plugin $plug
-				NOTHING_DELETED=false
-			    fi
-
-				isNotInDb=false
-				#echo $i # Put here the conditions
-			fi
-		done <<< "$(jq -c '.[]' <<< $research )"
-
-		# If the plugin is not in the wordpress db
-		if [[ "$isNotInDb" = true ]]
-		then
-		    delete_plugin $plug
-		    NOTHING_DELETED=false
-		fi	
-	elif [ $retval -eq 1 ]
-	then 
-		echo "Error : can't check for updates, probably due to connection"
-		exit 1
+	if [ $? -ne 0 ]; then # curl command failed, keep the plugin
+		echo "Non-zero curl exit code when fetching info for plugin ${plug}, skipping!"
+		continue
 	fi
-done <<< "$(wp plugin list --field=name)"
+
+	# Temporarily change the field separator (IFS) to split the API response into variables properly
+	IFS='|' read slug active_installs last_updated error <<<$(echo $resp | jq -r '[.slug, .active_installs, .last_updated, .error] | join("|")')
+
+	if [[ ! $error == "" ]]; then
+		if [ "$error" == "Plugin not found." ]; then
+			echo "Plugin ${plug} not found."
+			delete_plugin $plug
+			NOTHING_DELETED=false
+			continue
+		else
+			echo "Unknown WP API error when fetching info for plugin ${plug}: '$error'"
+			continue
+		fi
+	fi
+
+	d1=$(date --utc --date="$(date)" +"%Y-%m-%d %H:%M:%S")
+	d2=$(date --utc --date="${last_updated}+$TIMELAPSE" +"%Y-%m-%d %H:%M:%S")
+
+	if [[ "$d2" < "$d1" ]]; then # date criterion
+		echo "Plugin $plug hasn't received updates recently enough."
+		delete_plugin $plug
+		NOTHING_DELETED=false
+		continue
+	elif [[ $active_installs -lt $ACTIVEINSTALLS ]]; then # active installs criterion
+		echo "Plugin $plug doesn't have enough active installs."
+		delete_plugin $plug
+		NOTHING_DELETED=false
+		continue
+	else
+		echo "Plugin $plug ok!"
+	fi
+done <<<"$(wp plugin list --field=name)"
 [[ $? != 0 ]] && exit $?
 
 # If nothing has been deleted then success
--- stacks/wordpress-assos/wordpress/themes-remove.sh
+++ stacks/wordpress-assos/wordpress/themes-remove.sh
@@ -1,87 +1,82 @@
 #!/bin/bash
-# For each plugin check if it fits our criterions (updated less than 3y ago, more that 100 active_installs
-# For testing, use theme "default"
+# For each theme check if it fits our criterions (updated less than 6y ago, more that 1000 downloads
 
 TIMELAPSE="6 years"
-ACTIVEINSTALLS=1000
-DEFAULTTHEME=twentynineteen
+DOWNLOADS=1000
 
-# List of themes to be kept even if the conditions for removal are met
-#LIST_THEMES_KEPT="" #list in format "elem1 elem2 elem3" 
-# Should now be an environment variable, kept here for clarity
+# Fallback theme when the current one is invalid
+# Moved to a container environment variable
+# DEFAULT_THEME=twentytwentyfive
+
+# List of themes not to be deleted even if conditions are true
+#LIST_THEMES_KEPT="" 	# list in format "name1 name2 name3"
+# Should now be an environment variable, let here for the clarity
 
 declare -a NOTHING_DELETED
 NOTHING_DELETED=true
 
-delete_theme () {
-    echo "Warning ! Uninstalling theme $1"
-    wp theme is-active $1
-    act=$(echo $?)
-    if [[ $act -eq 0 ]]
-    then
-        echo "Current theme $1 is depreciated, changing for theme $DEFAULTTHEME"
-         wp theme install $DEFAULTTHEME
-         wp theme activate $DEFAULTTHEME
-    fi
-    wp theme delete "$1"
+delete_theme() {
+	echo "Warning ! Uninstalling theme $1"
+	wp theme is-active $1
+	act=$(echo $?)
+	if [[ $act -eq 0 ]]; then
+		echo "Current theme $1 is depreciated, changing for theme $DEFAULT_THEME"
+		wp theme install $DEFAULT_THEME
+		wp theme activate $DEFAULT_THEME
+	fi
+	wp theme delete "$1"
 }
 
+while read theme; do
 
-while read theme
-do
-	
-	# Skip the part of the list that we want to keep
-	for val in $LIST_THEMES_KEPT
-	do
-		if [ "${theme}x" = "${val}x" ]
-		then
-		    continue 2 # Next path
-		fi
+	# Skip the part of the list we want to keep
+	for val in $LIST_THEMES_KEPT; do
+		if [ "${theme}x" = "${val}x" ]; then
+			echo "Theme ${theme} in LIST_THEMES_KEPT, skipping."
+			continue 2 # Next theme
+		elif ["${theme}x" = "${DEFAULT_THEME}x"]; then
+			echo "Theme ${theme} is the default theme, skipping."
 	done
 
-	declare -a isNotInDb
-	isNotInDb=true
+	resp=$(curl -s "https://api.wordpress.org/themes/info/1.2/?action=theme_information&slug=${theme}")
 
-
-	research=`wp theme search $theme --fields=slug,last_updated,active_installs --format=json`
-	
-	retval=$?
-	# Checking if error (if so wp already prints something so we just prevent the deletion)
-	if [ $retval -eq 0 ]
-	then 
-		while read i;
-		do
-		    slug=$(echo `jq -r '.slug' <<< ${i}`)
-		    date=$(echo `jq -r '.last_updated' <<< ${i}`)
-		    active=$(echo `jq -r '.active_installs' <<< ${i}`)
-
-			if [[ "$slug" == "$theme" ]]
-			then
-			    d1=$(date --utc --date="$(date)" +"%Y-%m-%d %H:%M:%S")
-			    d2=$(date --utc --date="$date+$TIMELAPSE" +"%Y-%m-%d %H:%M:%S")
-
-			    if [[ "$d2" < "$d1" ]] || [[ $active -lt $ACTIVEINSTALLS ]] # date criterion
-			    then
-				delete_theme $theme
-				NOTHING_DELETED=false
-			    fi
-
-				isNotInDb=false
-			fi
-		done <<< $(jq -c '.[]' <<< $research )
-
-		# If the plugin is not in the wordpress db
-		if [[ "$isNotInDb" = true ]]
-		then
-		    delete_theme $theme
-		    NOTHING_DELETED=false
-		fi  	
-	elif [ $retval -eq 1 ]
-	then
-		echo "Error : can't check for updates, probably due to connection"
-		exit 1
+	if [ $? -ne 0 ]; then # curl command failed, keep the theme
+		echo "Non-zero curl exit code when fetching info for theme ${theme}, skipping!"
+		continue
 	fi
-done <<< "$(wp theme list --field=name)"
+
+	# Temporarily change the field separator (IFS) to split the API response into variables properly
+	IFS='|' read slug downloads last_updated error <<<$(echo $resp | jq -r '[.slug, .dowloads, .last_updated, .error] | join("|")')
+
+	if [[ ! $error == "" ]]; then
+		if [ "$error" == "Theme not found." ]; then
+			echo "Theme ${theme} not found."
+			delete_theme $theme
+			NOTHING_DELETED=false
+			continue
+		else
+			echo "Unknown WP API error when fetching info for theme ${theme}: '$error'"
+			continue
+		fi
+	fi
+
+	d1=$(date --utc --date="$(date)" +"%Y-%m-%d %H:%M:%S")
+	d2=$(date --utc --date="${last_updated}+$TIMELAPSE" +"%Y-%m-%d %H:%M:%S")
+
+	if [[ "$d2" < "$d1" ]]; then # date criterion
+		echo "Theme $theme hasn't received updates recently enough."
+		delete_theme $theme
+		NOTHING_DELETED=false
+		continue
+	elif [[ downloads -lt $DOWNLOADS ]]; then # downloads criterion
+		echo "Theme $theme doesn't have enough downloads."
+		delete_theme $theme
+		NOTHING_DELETED=false
+		continue
+	else
+		echo "Theme $theme ok!"
+	fi
+done <<<"$(wp theme list --field=name)"
 [[ $? != 0 ]] && exit $?
 
 # If nothing has been deleted then success
